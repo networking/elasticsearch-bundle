27881ba5f9ce0a7e605c5a9c30094d2c6fb6307f
Yorkie Chadwick <ychadwick@gmail.com>
2018-02-19 09:53:03 +0100

    update indexer and simplify checking

---
+++ src/Transformer/IndexableChecker.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: yorkie
+ * Date: 19.02.18
+ * Time: 09:36
+ */
+
+namespace Networking\ElasticSearchBundle\Transformer;
+
+
+use Networking\InitCmsBundle\Entity\Media;
+
+class IndexableChecker
+{
+    public static function isIndexable($object)
+    {
+        if ($object instanceof Media) {
+            if ($object->getContentType() == 'application/pdf') {
+                return $object->getEnabled();
+            }
+        }
+
+        return false;
+
+    }
+}
