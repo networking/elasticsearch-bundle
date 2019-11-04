e012e53c8caf8187bc8ad2d0db7af6f733c525d5
Yorkie Chadwick <ychadwick@gmail.com>
2019-11-04 13:48:00 +0100

    correct controller bugs

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,15 +2,25 @@
 
 namespace Networking\ElasticSearchBundle\Controller;
 
+use Elastica\Aggregation\Missing;
 use Elastica\Index;
 use Elastica\Query;
 use FOS\ElasticaBundle\Paginator\RawPaginatorAdapter;
 use Networking\ElasticSearchBundle\Query\BoolQuery;
 use Networking\InitCmsBundle\Controller\FrontendPageController;
 use Knp\Component\Pager\Paginator;
+use Knp\Component\Pager\PaginatorInterface;
+use Networking\InitCmsBundle\Helper\LanguageSwitcherHelper;
+use Networking\InitCmsBundle\Helper\PageHelper;
+use Networking\InitCmsBundle\Lib\PhpCacheInterface;
+use Networking\InitCmsBundle\Model\PageManagerInterface;
+use Sonata\AdminBundle\Admin\Pool;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\KernelInterface;
 use Symfony\Component\Routing\Exception\ResourceNotFoundException;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
 
 class DefaultController extends FrontendPageController
 {
@@ -22,12 +32,22 @@ class DefaultController extends FrontendPageController
     /**
      * @var string
      */
-	protected $baseTemplate;
+    protected $baseTemplate;
 
     /**
      * @var string
      */
-	protected $searchTemplate;
+    protected $searchTemplate;
+
+    /**
+     * @var PaginatorInterface
+     */
+    protected $paginator;
+
+    /**
+     * @var string
+     */
+    protected $environment;
 
     /**
      * DefaultController constructor.
@@ -36,11 +56,36 @@ class DefaultController extends FrontendPageController
      * @param $baseTemplate
      * @param $searchTemplate
      */
-    public function __construct(Index $index, $baseTemplate, $searchTemplate)
-    {
+    public function __construct(
+        PhpCacheInterface $phpCache,
+        TokenStorageInterface $tokenStorage,
+        AuthorizationCheckerInterface $authorizationChecker,
+        Pool $pool,
+        LanguageSwitcherHelper $languageSwitcherHelper,
+        PageManagerInterface $pageManager,
+        PageHelper $pageHelper,
+        Index $index,
+        PaginatorInterface $paginator,
+        KernelInterface $kernel,
+        $baseTemplate,
+        $searchTemplate
+    ) {
+        parent::__construct(
+            $phpCache,
+            $tokenStorage,
+            $authorizationChecker,
+            $pool,
+            $languageSwitcherHelper,
+            $pageManager,
+            $pageHelper
+        );
+        $this->environment = $kernel->getEnvironment();
+        $this->paginator = $paginator;
+        $this->pageHelper = $pageHelper;
         $this->index = $index;
         $this->baseTemplate = $baseTemplate;
         $this->searchTemplate = $searchTemplate;
+
     }
 
     /**
@@ -63,80 +108,80 @@ class DefaultController extends FrontendPageController
             $template = $template->getTemplate();
         }
 
-	    if ($params instanceof RedirectResponse) {
-		    return $params;
-	    }
+        if ($params instanceof RedirectResponse) {
+            return $params;
+        }
 
         $searchTerm = $request->query->get('search', false);
 
-	    $pagePaginator = false;
+        $pagePaginator = false;
 
-	    if ($searchTerm) {
+        if ($searchTerm) {
 
-		    $keywordQuery = new Query\QueryString( $searchTerm );
-		    $keywordQuery->setFields( [ 'content' ] )
-		                 ->setAnalyzeWildcard( true )
-		                 ->setPhraseSlop( 40 );
+            $keywordQuery = new Query\QueryString($searchTerm);
+            $keywordQuery->setFields(['content'])
+                ->setAnalyzeWildcard(true)
+                ->setPhraseSlop(40);
 
-		    $nameQuery = new Query\QueryString( $searchTerm );
-		    $nameQuery->setFields( [ 'name' ] )
-		              ->setAnalyzeWildcard( true )
-		              ->setPhraseSlop( 40 )
-		              ->setBoost( 2.0 );
-
-
-		    $metaTitle = new Query\QueryString( $searchTerm );
-		    $metaTitle->setFields( [ 'metaTitle' ] )
-		              ->setAnalyzeWildcard( true )
-		              ->setPhraseSlop( 40 );
-
-		    $disMax = new Query\DisMax();
-		    $disMax->addQuery( $nameQuery )
-		           ->addQuery( $keywordQuery )
-		           ->addQuery( $metaTitle )
-		           ->setTieBreaker( 0.3 );
-
-		    $query = new Query( $disMax );
+            $nameQuery = new Query\QueryString($searchTerm);
+            $nameQuery->setFields(['name'])
+                ->setAnalyzeWildcard(true)
+                ->setPhraseSlop(40)
+                ->setBoost(2.0);
 
 
-		    $localeQuery        = new Query\Match( 'locale', $request->getLocale() );
-		    $missingLocaleQuery = new Query\Missing( 'locale' );
+            $metaTitle = new Query\QueryString($searchTerm);
+            $metaTitle->setFields(['metaTitle'])
+                ->setAnalyzeWildcard(true)
+                ->setPhraseSlop(40);
 
-		    $or = new BoolQuery();
-		    $or->addShould( $localeQuery );
-		    $or->addShould( $missingLocaleQuery );
-		    $booleanQuery = new BoolQuery();
-		    $booleanQuery->addMust( $or );
+            $disMax = new Query\DisMax();
+            $disMax->addQuery($nameQuery)
+                ->addQuery($keywordQuery)
+                ->addQuery($metaTitle)
+                ->setTieBreaker(0.3);
+
+            $query = new Query($disMax);
 
 
-		    $query->setPostFilter( $booleanQuery );
-		    $request->query->set( 'search', $searchTerm );
+            $localeQuery = new Query\Match('locale', $request->getLocale());
 
-		    $query->setHighlight( [
-			    'fields' => [
-				    'content' => new \stdClass(),
-				    'name'    => new \stdClass(),
-			    ],
-		    ] );
+            $or = new BoolQuery();
+            $or->addShould($localeQuery);
+            $booleanQuery = new BoolQuery();
+            $booleanQuery->addMust($or);
 
-		    /** @var $paginator Paginator */
-		    $paginator   = $this->get( 'knp_paginator' );
-		    $currentPage = $request->query->get( 'page', 1 );
+            $missingLocaleAgg = new Missing('missing_locale', 'locale');
+            $query->addAggregation($missingLocaleAgg);
 
-		    $paginatorAdaptor = new \Networking\ElasticSearchBundle\Paginator\RawPaginatorAdapter( $this->index, $query );
-		    /** @var \Knp\Bundle\PaginatorBundle\Pagination\SlidingPagination $pagePaginator */
-		    $pagePaginator = $paginator->paginate( $paginatorAdaptor, $currentPage );
-		    $pagePaginator->setParam( 'search', $searchTerm );
-		    $pagePaginator->setUsedRoute( 'site_search_' . substr( $request->getLocale(), 0, 2 ) );
-		    $pagePaginator->setTemplate( '@NetworkingElasticSearch/Pagination/twitter_bootstrap_pagination.html.twig' );
-	    }
+            $query->setPostFilter($booleanQuery);
+            $request->query->set('search', $searchTerm);
+
+            $query->setHighlight(
+                [
+                    'fields' => [
+                        'content' => new \stdClass(),
+                        'name' => new \stdClass(),
+                    ],
+                ]
+            );
+
+            $currentPage = $request->query->get('page', 1);
+
+            $paginatorAdaptor = new \Networking\ElasticSearchBundle\Paginator\RawPaginatorAdapter($this->index, $query);
+            /** @var \Knp\Bundle\PaginatorBundle\Pagination\SlidingPagination $pagePaginator */
+            $pagePaginator = $this->paginator->paginate($paginatorAdaptor, $currentPage);
+            $pagePaginator->setParam('search', $searchTerm);
+            $pagePaginator->setUsedRoute('site_search_'.substr($request->getLocale(), 0, 2));
+            $pagePaginator->setTemplate('@NetworkingElasticSearch/Pagination/twitter_bootstrap_pagination.html.twig');
+        }
 
         $params = array_merge(
             $params,
             [
                 'paginator' => $pagePaginator,
-                'search_term' => $searchTerm?explode(' ', trim($searchTerm)):false,
-                'url_prefix' => $this->get('kernel')->getEnvironment() == 'dev' ? '/app_dev.php' : '',
+                'search_term' => $searchTerm ? explode(' ', trim($searchTerm)) : false,
+                'url_prefix' => $this->environment == 'dev' ? '/app_dev.php' : '',
                 'base_template' => $this->baseTemplate,
             ]
         );
